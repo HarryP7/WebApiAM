81894cd8cc03a4bb3ef3bf634bdd39a8e89a78a6
HarryP7 <gish177@rambler.ru>
2019-02-01 01:57:48 +0300

    added validation and column types

--- WebApiAM/Models/RegisterUserViewModel.cs
+++ WebApiAM/Models/RegisterUserViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,10 +8,15 @@ namespace WebApiAM.Models
 {
     public class RegisterUserViewModel
     {
+        [Required(ErrorMessage = "Введите имя пользователя"), Display(Name = "Имя пользователя")]
         public string FullName { get; set; }
+        [Required(ErrorMessage = "Введите пароль")]
         public string Password { get; set; }
+        [Required(ErrorMessage = "Введите Email")]
         public String Email { get; set; }
+        [Required(ErrorMessage = "Введите телефон"), Display(Name = "Телефон")]
         public String Phone { get; set; }
+        [Required(ErrorMessage = "Введите Login")]
         public String Login { get; set; }
         public int RoleId { get; set; }
 
--- WebApiAM/Models/User.cs
+++ WebApiAM/Models/User.cs
@@ -1,18 +1,31 @@
 using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace WebApiAM.Models
 {
     public class User
     {
         public User() { }
+        [Key]
         public int Id { get; set; }
+        public String Uid { get; set; }
+        [Display(Name = "Имя пользователя"), Column(TypeName = "varchar(100)")]
         public String FullName { get; set; }
+        [Column(TypeName = "varchar(50)")]
         public String Email { get; set; }
+        [Display(Name = "Телефон"), Column(TypeName = "varchar(16)")]
         public String Phone { get; set; }
+        [Column(TypeName = "varchar(30)")]
         public String Login { get; set; }
+        [ForeignKey(nameof(Role)), Display(Name = "Роль")]
         public int RoleId { get; set; }
-        public Role Role { get; set; }
         public byte[] PasswordHash { get; set; }
         public byte[] PasswordSalt { get; set; }
+
+        [InverseProperty(nameof(Event.User))]
+        public virtual ICollection<Event> Events { get; set; } = new HashSet<Event>();
+        public virtual Role Role { get; set; }
     }
 }
