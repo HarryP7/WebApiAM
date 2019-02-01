ef437b705815f47d239b8750927f8de12d59e7c6
HarryP7 <gish177@rambler.ru>
2019-02-02 01:54:11 +0300

    Added ErrorMessages to AuthUserViewModel

--- WebApiAM/Models/AuthUserViewModel.cs
+++ WebApiAM/Models/AuthUserViewModel.cs
@@ -1,9 +1,14 @@
 using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace WebApiAM.Models
 {
     public class AuthUserViewModel
     {
+        [Required(ErrorMessage = "Не указан Email")]
         public string Email { get; set; }
+        [Required(ErrorMessage = "Не указан пароль")]
+        [DataType(DataType.Password)]
         public string Password { get; set; }
     }
 }
