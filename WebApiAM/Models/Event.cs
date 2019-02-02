d967731d50f46c2cc8fe356055c3a593601ab520
HarryP7 <gish177@rambler.ru>
2019-02-02 23:19:48 +0300

    Moved some fields to class Service

--- WebApiAM/Models/Event.cs
+++ WebApiAM/Models/Event.cs
@@ -12,21 +12,16 @@ namespace WebApiAM.Models
         [Key]
         public int Id { get; set; }
         public String Uid { get; set; } = Guid.NewGuid().ToString("D");
-        [Display(Name = "Широта"), Column(TypeName = "decimal(10, 6)")]
-        public Decimal Lat { get; set; }
-        [Display(Name = "Долгота"), Column(TypeName = "decimal(10, 6)")]
-        public Decimal Lng { get; set; }
-        [Display(Name = "Название"), Column(TypeName = "varchar(100)")]
-        public String Title { get; set; }
-        [Display(Name = "Дата размещения")]
-        public DateTime DatePlace { get; set; }
         [Display(Name = "Дата события")]
         public DateTime EvDate { get; set; }
         [Display(Name = "Стоимость"), Column(TypeName = "decimal(6, 2)")]
         public Decimal Cost { get; set; }
-        [ForeignKey(nameof(User)), Display(Name = "Пользователь")]
+        [Display(Name = "Статус")]
+        public EventStatus Status { get; set; }
+        [Display(Name = "Комментарий"), Column(TypeName = "varchar(MAX)")]
+        public string Comment { get; set; }
+        [Display(Name = "Пользователь"), ForeignKey(nameof(User))]
         public int Fk_user { get; set; }
-
         public virtual User User { get; set; }
     }
 }
