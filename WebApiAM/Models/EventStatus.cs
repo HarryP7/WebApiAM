cabfb1e6ae5df5e123579fd498a9c5a034887472
HarryP7 <gish177@rambler.ru>
2019-02-02 23:21:14 +0300

    added enum EventStatus

---
+++ WebApiAM/Models/EventStatus.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiAM.Models
+{
+    public enum EventStatus
+    {
+        Create,//Создан
+        Edit,//Редактирование
+        Joined,//Присоединились
+        Canceled,//Отменен
+        Live,//Идет сейчас
+        Completed//Завершен
+    }
+}
