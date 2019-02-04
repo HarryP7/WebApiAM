9679bdad8126370c3d1c4127a70daa91eb4a2864
HarryP7 <gish177@rambler.ru>
2019-02-04 23:44:17 +0300

    some changes in UserService

--- WebApiAM/Models/Services/UserService.cs
+++ WebApiAM/Models/Services/UserService.cs
@@ -4,16 +4,15 @@ using WebApiAM.Models;
 using WebApiAM.Helpers;
 using WebApiAM.Repository;
 using System.Linq.Expressions;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
 
 namespace WebApiAM.Services
 {
     public class UserService : IUserService
     {
-        private readonly IRepository<User> userRepo;
         private readonly AppDbContext context;
-        public UserService(IRepository<User> userRepo, AppDbContext context)
-        {
-            this.userRepo = userRepo;
+        public UserService(AppDbContext context){
             this.context = context;
         }
 
@@ -22,7 +21,7 @@ namespace WebApiAM.Services
             if (string.IsNullOrEmpty(email) || string.IsNullOrEmpty(password))
                 return null;
 
-            var user = userRepo.Get(u => u.Email == email);
+            var user = Get(u => u.Email == email);
 
             // check if username exists and password is correct
             if (user == null || !VerifyPasswordHash(password, user.PasswordHash, user.PasswordSalt))
@@ -37,7 +36,7 @@ namespace WebApiAM.Services
             if (string.IsNullOrWhiteSpace(password))
                 throw new AppException("Необходим пароль");
 
-            if (userRepo.Get(u => u.Email == user.Email) != null)
+            if (Get(u => u.Email == user.Email) != null)
                 throw new AppException($"Пользователь с электронной почтой {user.Email} уже существует.");
             
             byte[] passwordHash, passwordSalt;
@@ -46,35 +45,35 @@ namespace WebApiAM.Services
             user.PasswordHash = passwordHash;
             user.PasswordSalt = passwordSalt;
 
-            userRepo.Create(user);
+            context.Add(user);
             context.SaveChanges();
 
             return user;
         }
-
-        public void Delete(int id)
-        {
-            userRepo.Delete(new User() { Id = id });
-        }
-
-        public IEnumerable<User> Fetch(Expression<Func<User, bool>> predicate)
-        {
-            return userRepo.Fetch(predicate);
-        }
-
         public User Get(int id)
         {
-            return userRepo.Get(id);
-        }
-
+            try{ return context.Users.Where(u => u.Id == id).Single(); }
+            catch{ return null; }
+        }     
         public User Get(Func<User, bool> predicate)
         {
-            throw new NotImplementedException();
+            try{ return context.Users.Where(predicate).Single(); }
+            catch{ return null; }
         }
 
         public void Update(User user, string password = null)
         {
             throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            context.Remove(new User() { Id = id });
+        }
+
+        public IEnumerable<User> Fetch(Expression<Func<User, bool>> predicate)
+        {
+            return context.Users.Where(predicate).Include(u => u.Role);
         }
 
         private static void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt){
@@ -87,14 +86,15 @@ namespace WebApiAM.Services
                 passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
             }
         }
-
         private static bool VerifyPasswordHash(string password, byte[] storedHash, byte[] storedSalt)
         {
             if (password == null) throw new ArgumentNullException("password");
-            if (string.IsNullOrWhiteSpace(password)) throw new ArgumentException("Value cannot be empty or whitespace only string.", "password");
-            if (storedHash.Length != 64) throw new ArgumentException("Invalid length of password hash (64 bytes expected).", "passwordHash");
-            if (storedSalt.Length != 128) throw new ArgumentException("Invalid length of password salt (128 bytes expected).", "passwordHash");
-
+            if (string.IsNullOrWhiteSpace(password))
+                throw new ArgumentException("Value cannot be empty or whitespace only string.", "password");
+            if (storedHash.Length != 64)
+                throw new ArgumentException("Invalid length of password hash (64 bytes expected).", "passwordHash");
+            if (storedSalt.Length != 128)
+                throw new ArgumentException("Invalid length of password salt (128 bytes expected).", "passwordHash");
             using (var hmac = new System.Security.Cryptography.HMACSHA512(storedSalt))
             {
                 var computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
@@ -103,9 +103,7 @@ namespace WebApiAM.Services
                     if (computedHash[i] != storedHash[i]) return false;
                 }
             }
-
             return true;
         }
-
     }
 }
